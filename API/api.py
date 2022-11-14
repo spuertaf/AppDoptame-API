cf3900b54cdda0c24988c21ea31ae5e78aa7234d
Santiago Puerta <puertasantiago2002@gmail.com>
2022-11-14 16:32:56 -0500

    añado ruta para recomendacion de razas de perros

--- API/api.py
+++ API/api.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from usable_model import prediction
 from flask import request
+from liked_pets import top3_liked_pets_by_user
+
+
 
 '''
-appdoptame : nombre de la api
-/appdoptame/dog-race : ruta de la api para reconocimiento de raza de un perro
+/dog-race : ruta de la api para reconocimiento de raza de un perro
+/recommendation : ruta de la api para recomendacion de razas de perros
 '''
-
 
 
 
 if __name__ == '__main__':
+
+
     
     app = Flask(__name__)
+
+
 
     @app.route("/dograce") #ruta de la api para deteccion de raza de un perro
     #una vez se acceda a la ruta se va a ejecutar esto
@@ -26,4 +32,15 @@ if __name__ == '__main__':
             print(error)
             predict = f"STATUS CODE: 400 \nError en la prediccion"
         return {"race":predict}
+    
+    
+    
+    @app.route("/recommendation") #ruta de la api para recomendacion de mascota
+    def get_recommendation():
+        user_id = request.args.get('user_id', default =1, type = str)
+        response = top3_liked_pets_by_user(user_id, "")
+        return response
+    
+
+
     app.run(host = '0.0.0.0',port = 80, debug = True) 
