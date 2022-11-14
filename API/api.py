94d9d479aa0bf254d161fba9bb5433acbb7fba3b
Santiago Puerta <puertasantiago2002@gmail.com>
2022-11-14 17:05:47 -0500

    añado rama para recomendacion de razas de perros

--- API/api.py
+++ API/api.py
@@ -20,18 +20,11 @@ if __name__ == '__main__':
 
 
 
-    @app.route("/dograce") #ruta de la api para deteccion de raza de un perro
-    #una vez se acceda a la ruta se va a ejecutar esto
-    def model_prediction():
-        try:
-            print("Prediciendo la raza de la foto...")
-            img_path = request.args.get('url', default = 1, type = str)
-            predict = prediction(img_path)
-            print(f"La raza es {predict}")
-        except Exception as error:
-            print(error)
-            predict = f"STATUS CODE: 400 \nError en la prediccion"
-        return {"race":predict}
+    @app.route("/recommendation") #ruta de la api para recomendacion de mascota
+    def get_recommendation():
+        user_id = request.args.get('user_id', default =1, type = str)
+        response = top3_liked_pets_by_user(user_id, "credentials.json")
+        return response
     
 
 
